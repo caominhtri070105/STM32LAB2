a83e8710f6f95bd797926a2b8a936d3cbcfbde27
caominhtri070105 <caominhtri070105@gmail.com>
2025-10-05 19:19:21 +0700

    Init for Ex9

--- LAB2_Exercise/Core/Src/software_timer.c
+++ LAB2_Exercise/Core/Src/software_timer.c
@@ -13,6 +13,9 @@ int timer1_flag = 0;
 int timer2_counter = 0;
 int timer2_flag = 0;
 
+int timer3_counter = 0;
+int timer3_flag = 0;
+
 void setTimer1(int duration){
 	timer1_counter = duration/TIMER_CYCLE;
 	timer1_flag = 0;
@@ -20,6 +23,10 @@ void setTimer1(int duration){
 void setTimer2(int duration){
 	timer2_counter = duration/TIMER_CYCLE;
 	timer2_flag = 0;
+}
+void setTimer3(int duration){
+	timer3_counter = duration/TIMER_CYCLE;
+	timer3_flag = 0;
 }
 void timerRun(){
 	if(timer1_counter > 0){
@@ -32,6 +39,12 @@ void timerRun(){
 			timer2_counter--;
 			if(timer2_counter <=0){
 				timer2_flag = 1;
+			}
+		}
+	if(timer3_counter > 0){
+			timer3_counter--;
+			if(timer3_counter <=0){
+				timer3_flag = 1;
 			}
 		}
 }
